d7a0c4c89f99f7fab5d21e475e5fb150d5fb7109
LlucSerramitjana <80621909+LlucSerramitjana@users.noreply.github.com>
2021-09-28 17:10:29 +0200

    Add files via upload

---
+++ proyecto/app/models/Calendari.java
@@ -0,0 +1,17 @@
+package models;
+import javax.persistence.*;
+
+import play.db.jpa.*;
+@Entity
+public class Calendari extends Model{
+    public String titulo;
+    public int id;
+    @ManyToOne
+    public User User;
+    public Calendari(String titulo, int id, models.User User){
+        this.titulo = titulo;
+        this.id = id;
+        this.User = User;
+    }
+
+}
