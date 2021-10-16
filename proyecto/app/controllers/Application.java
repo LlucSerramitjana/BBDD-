538f4d8bed49384e291332509dfd8006f413f18b
LlucSerramitjana <80621909+LlucSerramitjana@users.noreply.github.com>
2021-10-16 17:32:53 +0200

    Add files via upload

--- proyecto/app/controllers/Application.java
+++ proyecto/app/controllers/Application.java
@@ -2,15 +2,27 @@ package controllers;
 
 import play.*;
 import play.mvc.*;
+import play.db.jpa.JPA;
 
 import java.util.*;
 
 import models.*;
 
-public class Application extends Controller {
+import javax.persistence.Query;
 
-    public static void index() {
-        render();
+public class Application extends Controller {
+    public static void login(String Name, String Pwd){
+        User us = User.find("byFullnameAndPassword", Name, Pwd).first();
+        if(us != null){
+            renderText("login " + Name);
+        }
+        else
+            renderText("Abans registrar-se");
+    }
+
+    public static void index(int i) {
+        new User(1234, "pwd", "Lluc").save();
+        new User(2345, "pwd1", "Christian").save();
     }
 
 }
